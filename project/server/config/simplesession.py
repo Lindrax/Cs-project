30e9578fef6f0d0549ef455c8c118edcb9778f8b
Axel <->
2024-07-07 16:09:26 +0300

    sessioncookie

--- project/server/config/simplesession.py
+++ project/server/config/simplesession.py
@@ -1,9 +1,16 @@
 import random
 import django.contrib.sessions.backends.db as db
+import os
 
 class SessionStore(db.SessionStore):
     def _get_new_session_key(self):
         while True:
             session_key = 'session-' + str(random.randint(1, 10))
             if not self.exists(session_key):
+                return 
+            
+        """while True:
+            session_key = os.urandom(16).hex()
+            if not self.exists(session_key):
                 return session_key
+                """
